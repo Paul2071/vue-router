aa603f06cc4b83e15cfe8be409c99259008165c2
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-07-10 16:52:37 +0100

    another page and routing to it added

---
+++ src/views/Paints.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Paints</h1>
+    <div v-for="paint in paints" :key="paint.id" >
+   <h2>{{ paint.name }}</h2> 
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      paints: [
+        { name: "Striking Scorpion Green", id: 1, type: "contrast" },
+        { name: "Doomfire Magenta", id: 2, type: "contrast" },
+        { name: "Kroxigor Scales", id: 3, type: "contrast" },
+      ],
+    };
+  },
+};
+</script>
