a1773048443cb00d2b851c70bde94ecd7442a456
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-07-13 19:04:30 +0100

    added styling to paints comp

--- src/views/paints/Paints.vue
+++ src/views/paints/Paints.vue
@@ -1,13 +1,34 @@
 <template>
     <h1>Paints</h1>
-    <div v-for="paint in paints" :key="paint.id" >
+    <div v-for="paint in paints" :key="paint.id" class="paint" >
    <router-link :to="{ name : 'PaintDetails', params: { id: paint.id } }">
     <h2>{{ paint.name }}</h2> 
    </router-link>
     </div>
 </template>
 
+<style>
+
+.paint h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444
+}
+.paint h2:hover {
+  background: #ddd
+}
+.paint a {
+  text-decoration: none;
+}
+
+
+</style>
 <script>
+
 export default {
   data() {
     return {
