749161d2b4b2d0771b65398fde5a65f209bf66a1
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-07-20 23:25:20 +0100

    added json server package to add endpoints

--- src/views/paints/Paints.vue
+++ src/views/paints/Paints.vue
@@ -33,9 +33,7 @@ export default {
   data() {
     return {
       paints: [
-        { name: "Striking Scorpion Green", id: 1, type: "contrast" },
-        { name: "Doomfire Magenta", id: 2, type: "contrast" },
-        { name: "Kroxigor Scales", id: 3, type: "contrast" },
+       
       ],
     };
   },
