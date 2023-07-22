538aedb09cf1a47542779616fef7e72131c6ecb5
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-07-22 10:37:25 +0100

    fetch data on click of paint to get the paint details added using v-if to conditionally render as the fetch changes the v-if to true therefore it renders

--- src/views/paints/PaintDetails.vue
+++ src/views/paints/PaintDetails.vue
@@ -1,15 +1,28 @@
 <template>
-    <div class="paintdetails">
-      <h1> paintdetails</h1>
+    <div v-if="paint">
+      <h1> {{ paint.name }}</h1>
+      <h2> {{ paint.type }}</h2>
       <p> {{ id }}</p>
-      <p> {{ type }}</p>
     </div>
+    
   </template>
   
   <script>
   
   export default {
     props: ['id'],
+    data(){
+      return {
+        paint: null
+      }
+    },
+   
+    mounted () {
+    fetch("http://localhost:3000/painting/" + this.id) 
+    .then( res => res.json() )
+    .then( data => this.paint = data)
+    .catch( err => console.log(err.message))
+  }
   
   }
   </script>
